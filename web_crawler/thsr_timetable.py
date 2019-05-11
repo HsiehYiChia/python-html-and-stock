44287452d60fc6cbe3afd75f8898bf34ce96f28d
ychsieh <lhsieh0107@gmail.com>
2019-05-11 18:25:12 +0800

    add get_station_hash() for post request form data

--- web_crawler/thsr_timetable.py
+++ web_crawler/thsr_timetable.py
@@ -1,9 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-req = requests.get('https://www.thsrc.com.tw/index.html')
-soup = BeautifulSoup(req.text, 'html.parser')
+thsr_home_url = 'https://www.thsrc.com.tw/index.html'
+thsr_timetable_url = 'https://www.thsrc.com.tw/tw/TimeTable/Search'
 
-divs = soup.find_all(class_ ='revision04_title')
-for div in divs:
-    print(div.get("title"))
+
+def get_station_hash():
+    req = requests.get('https://www.thsrc.com.tw/tw/TimeTable/SearchResult')
+    encode = req.apparent_encoding
+    req.encoding = str(encode)
+    soup = BeautifulSoup(req.text, 'html.parser')
+    stations = soup.find('select', id='StartStation')
+    stations = stations.find_all('option')
+    for sta in stations:
+        print(sta['value'], sta.text)
+    
+
+get_station_hash()
+
+formdata = {} 
+req = requests.post(thsr_timetable_url, data=formdata)
